0f852c65f465136fe4a443988c338cf94a62866d
Manuel Fuentes Torres <fuentesempresa@gmail.com>
2025-11-04 08:26:35 +0100

    Actualizar version de upload-pages-artifact

--- iniciar-dev.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Iniciando el servidor de desarrollo...
-
-:: Abrir el navegador en la página de admin
-start "Panel de Administracion" "http://localhost:3000/admin"
-
-:: Iniciar el servidor de Node.js
-echo Para detener el servidor, cierra esta ventana o pulsa CTRL+C.
-npm run dev
